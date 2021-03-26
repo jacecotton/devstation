1a6b90fd99c5b33a8e3ca0c79ef69d61c46a0455
Jace Cotton <jacecotton@Jaces-MacBook-Pro.local>
2021-03-26 01:19:14 -0500

    minor update

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <header className="site-header">
+      <h1 className="site-name">
+        <a href="/">DevStation</a>
+      </h1>
+    </header>
+  )
+}
+
+export default Header
