af8ec08ab003b50257d5fc7c22f4cbfe74c52e79
Jace Cotton <jacecotton@Jaces-MacBook-Pro.local>
2021-03-31 21:25:12 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import Header from "./components/Header"
+import React from "react";
+import Signup from "./components/Signup";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <a href="/app">Test link</a>
+      
     </div>
   )
 }
